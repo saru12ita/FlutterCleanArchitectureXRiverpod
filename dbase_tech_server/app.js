6a84199bf49f2f4f331b664e8640e51a0f1e08c5
sarita bk <sarita.211732@ncit.edu.np>
2025-05-04 18:49:54 +0545

    new datas

--- dbase_tech_server/app.js
+++ dbase_tech_server/app.js
@@ -1,29 +1,50 @@
 
-const express =require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // ✅ Correct variable name for CORS
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 require('dotenv/config');
 
+
+
 const app = express();
-const env= process.env;
+const env = process.env;
 
-app.get('/',(req,res)=>{
-  return res.send('<h1>WELCOME TO SARITA CITY </h1>')
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.options('*', cors());
+
+
+
+app.get('/', (req, res) => {
+  return res.status(404).send('Sorry, can\'t find that');
+
 });
 
-app.get('/watch/videos/:id',(request,response)=>{
-
-    return response.json({'videoId': request.params.id})
-
-
+app.get('/watch/videos/:id', (request, response) => {
+  return response.json({ videoId: request.params.id });
 });
+
+
+
 
 // Start the server
 //localhost >> 192.168.0.1
-const hostname =env.HOST;
-const port=env.PORT;
+const hostname = env.HOST;
+const port = env.PORT;
 
 
-app.listen(port, hostname, ()=>{
-    console.log(`server running at http://${hostname}:${port}`);
+mongoose.connect(env.MONGODB_CONNECTION_STRING).then(() => {
+  console.log('Connetcted to Database');
+}).catch((error) => {
+  console.error(error);
+
+});
+
+app.listen(port, hostname, () => {
+  console.log(`server running at http://${hostname}:${port}`);
 });
 
 // C R U D
