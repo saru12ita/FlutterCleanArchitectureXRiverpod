39e2a16a8308e9a0f04eb8766e071277f8aca120
sarita bk <sarita.211732@ncit.edu.np>
2025-05-04 11:11:35 +0545

    Updated app.js and package files, added .env

--- dbase_tech_server/app.js
+++ dbase_tech_server/app.js
@@ -1,12 +1,41 @@
-const express =require('express');
 
-const app = express()
+const express =require('express');
+require('dotenv/config');
+
+const app = express();
+const env= process.env;
+
+app.get('/',(req,res)=>{
+  return res.send('<h1>WELCOME TO SARITA CITY </h1>')
+});
+
+app.get('/watch/videos/:id',(request,response)=>{
+
+    return response.json({'videoId': request.params.id})
+
+
+});
 
 // Start the server
 //localhost >> 192.168.0.1
-const hostname ='0.0.0.0';
-const port=3000;
+const hostname =env.HOST;
+const port=env.PORT;
+
+
 app.listen(port, hostname, ()=>{
     console.log(`server running at http://${hostname}:${port}`);
 });
+
+// C R U D
+//CREATE DATA
+//app.post('/register')
+//READ THE DATA
+//app.get('/')
+//UPDATE THE DATA
+//app.put()
+//DELETE THE DATA
+//app.delete()
+
+//http.post('/file')
+
 
